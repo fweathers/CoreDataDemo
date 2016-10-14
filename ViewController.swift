1af0739f0b54717fdf672ba23889092790b8a300
Felicia Weathers <Fel.Weathers@gmail.com>
2016-10-14 01:01:32 -0400

    Set up new user info

--- ViewController.swift
+++ ViewController.swift
@@ -13,7 +13,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext // use context to access CoreData, save or restore data from the entity that was created
+        
+        let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
+        newUser.setValue("Felicia", forKey: "username")
+        newUser.setValue("password1", forKey: "password")
+        newUser.setValue(99, forKey: "age")
+    
     }
 
     override func didReceiveMemoryWarning() {
