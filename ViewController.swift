70309ca70f0d9b82ba3b8a8a5c26dd08ff1f3b3d
Felicia Weathers <Fel.Weathers@gmail.com>
2016-10-14 00:44:13 -0400

    Set up Core Data Entity and Attribute

---
+++ ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Felicia Weathers on 10/14/16.
+//  Copyright © 2016 Felicia Weathers. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
